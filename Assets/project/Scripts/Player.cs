bcb28b81bd1cbdcb16266c0d0f4a7da5894ad0bb
Max Schafer <maxsg5@hotmail.com>
2020-12-22 20:16:01 -0700

    Section6 PlayerMovement completed

--- Assets/project/Scripts/Player.cs
+++ Assets/project/Scripts/Player.cs
@@ -4,43 +4,80 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float jumpingForce = 150;
+    [Header("Visuals")]
+    public GameObject model;
 
+    public float rotatingSpeed = 2f;
+
+    [Header("Movement")]
+    public float movingVelocity = 10;
+
+    public float jumpingVelocity = 150;
+
+    private Rigidbody playerRigidbody;
     private bool canJump;
+    private Quaternion targetModelRoatation;
 
     // Start is called before the first frame update
     private void Start()
     {
+        playerRigidbody = GetComponent<Rigidbody>();
+        targetModelRoatation = Quaternion.Euler(0, 0, 0);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // Raycast to identify if the player can jump
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.01f))
+        {
+            canJump = true;
+        }
+        model.transform.rotation = Quaternion.Lerp(model.transform.rotation, targetModelRoatation,
+            Time.deltaTime * rotatingSpeed);
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
+        // Move in the XZ plane.
         //move right
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * 5f * Time.deltaTime;
+            playerRigidbody.velocity = new Vector3(movingVelocity, playerRigidbody.velocity.y,
+                playerRigidbody.velocity.z);
+            targetModelRoatation = Quaternion.Euler(0, 270, 0);
         }
         //move left
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * 5f * Time.deltaTime;
+            playerRigidbody.velocity = new Vector3(-movingVelocity, playerRigidbody.velocity.y,
+                playerRigidbody.velocity.z);
+            targetModelRoatation = Quaternion.Euler(0, 90, 0);
         }
         //move forward
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * 5f * Time.deltaTime;
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, playerRigidbody.velocity.y,
+                movingVelocity);
+            targetModelRoatation = Quaternion.Euler(0, 180, 0);
         }
         //move backwards
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * 5f * Time.deltaTime;
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, playerRigidbody.velocity.y,
+                -movingVelocity);
+            targetModelRoatation = Quaternion.Euler(0, 0, 0);
         }
 
-        if (Input.GetKeyDown("space"))
+        //jump up
+        if (canJump && Input.GetKeyDown("space"))
         {
-            if (canJump)
-                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingForce);
+            canJump = false;
+            playerRigidbody.AddForce(Vector3.up * jumpingVelocity);
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, jumpingVelocity, playerRigidbody.velocity.z);
         }
     }
 }
