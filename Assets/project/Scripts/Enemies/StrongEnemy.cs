150ce6b68704a16f6de8ed5aef97721e19826439
Max Schafer <maxsg5@hotmail.com>
2020-12-24 18:34:07 -0700

    left off on strongEnemy

---
+++ Assets/project/Scripts/Enemies/StrongEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongEnemy : Enemy
+{
+    public override void Hit()
+    {
+        base.Hit();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
